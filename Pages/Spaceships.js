9442f91266c9b5d7173c2c23a1f6546d974e9579
adragon9 <adragon9@outlook.com>
2024-11-17 13:40:41 -0600

    Added search bar

--- Pages/Spaceships.js
+++ Pages/Spaceships.js
@@ -2,15 +2,15 @@ import React from "react";
 import { View, Text, StatusBar, ScrollView } from "react-native";
 import styles from "../styles";
 import CallAPI from "../components/api";
+import Input from "../components/searchBar";
 
 export default function Spaceships({ navigation }) {
   return (
     <View style={styles.container}>
       <StatusBar barStyle="dark-content" />
       <Text style={styles.Header_1}>Spaceships Screen</Text>
-      <View>
-        {CallAPI("https://www.swapi.tech/api/starships")}
-      </View>
+      <Input />
+      {CallAPI("https://www.swapi.tech/api/starships")}
     </View>
   );
 }
